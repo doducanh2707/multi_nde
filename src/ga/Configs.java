18b33d83dd42fe6340ac44760794ea52ca4ba8e6
doducanh2707 <doducanh2707@gmail.com>
2022-12-05 15:15:08 +0700

    .

--- src/ga/Configs.java
+++ src/ga/Configs.java
@@ -6,7 +6,7 @@ public class Configs {
 	public static Random rd;
 	public static int MAX_VALUE = Integer.MAX_VALUE/3;
 	public static int POPULATION_SIZE = 200;
-	public static int MAX_GENERATIONS = 500;
+	public static int MAX_GENERATIONS = 100;
 	public static int REPEAT = 30;
 	public static double CROSSOVER_RATE = 0.8;
 	public static double MUTATION_RATE = 0.1;
